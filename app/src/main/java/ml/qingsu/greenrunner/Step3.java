9badd585fd37be95581c79dd91413edf6a0faf26
w568w <1278297578@qq.com>
2017-06-20 18:40:48 +0800

    Null Pointer Bug Fixed

--- app/src/main/java/ml/qingsu/greenrunner/Step3.java
+++ app/src/main/java/ml/qingsu/greenrunner/Step3.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.content.pm.ServiceInfo;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
@@ -20,17 +21,19 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Step3 extends WizardStep {
     TextView tv;
-    private static final String TAG="aa";
+    private static final String TAG = "aa";
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         tv = new TextView(MyApplication.getInstance());
         tv.setText(R.string.step3);
-        tv.setPadding(5,5,5,5);
+        tv.setPadding(5, 5, 5, 5);
         Log.d(TAG, "onCreateView() called with: " + "inflater = [" + inflater + "], container = [" + container + "], savedInstanceState = [" + savedInstanceState + "]");
         return tv;
     }
@@ -39,7 +42,7 @@ public class Step3 extends WizardStep {
     public void setUserVisibleHint(boolean isVisibleToUser) {
         super.setUserVisibleHint(isVisibleToUser);
         Log.d(TAG, "setUserVisibleHint() called with: " + "isVisibleToUser = [" + isVisibleToUser + "]");
-             if (isVisibleToUser) {
+        if (isVisibleToUser) {
             notifyIncomplete();
             new Thread(new Runnable() {
                 @Override
@@ -103,42 +106,53 @@ public class Step3 extends WizardStep {
                     sb.append("</p><hr />");
                     //=============================
                     sb.append("<p>(省电)Alarm限制&emsp;");
-                    if(ShellUtils.isRoot())
-                    {
-                        ShellUtils.CommandResult cr=ShellUtils.execCommand("dumpsys alarm",true,true);
-                        if(cr.result==0)
-                        {
-                            String top=String_Between(cr.successMsg,"Top Alarms:","Alarm Stats:");
-                            if(!top.contains(Step2.selected.packageName))
-                            {
+                    if (ShellUtils.isRoot()) {
+                        ShellUtils.CommandResult cr = ShellUtils.execCommand("dumpsys alarm", true, true);
+                        if (cr.result == 0) {
+                            String top = String_Between(cr.successMsg, "Top Alarms:", "Alarm Stats:");
+                            if (!top.contains(Step2.selected.packageName)) {
                                 sb.append("Passed");
                                 good++;
-                            }
-                            else
-                            {
+                            } else {
                                 sb.append("Failed");
                             }
-                        }
-                        else
-                        {
+                        } else {
                             good++;
                             sb.append("Root执行失败,无法检测");
                         }
-                    }
-                    else
-                    {
+                    } else {
                         good++;
                         sb.append("本应用没有Root权限,无法检测");
                     }
                     sb.append("</p><hr />");
-
-                    sb.append("<h2>综上所述，评定 "+Step2.selected.appName+" "+good*20+"%符合绿色公约要求。</h2><hr /><a href=\"http://green-android.org\">绿色公约官网</a><p>By w568w</p>");
+                    //========================
+                    sb.append("<p>(优化、隐私)广告&emsp;");
+                    ArrayList<String> al = getActivities(Step2.selected.packageName);
                     try {
-                        File_WritetoSDFrom_bytes(sb.toString().getBytes("UTF-8"),"result.html");
+                        Step2.selected.packageInfo = pm.getPackageInfo(Step2.selected.packageName, PackageManager.GET_SERVICES);
+
+                    } catch (PackageManager.NameNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    if (contains(al, "Ad") || contains(al, "AD") || contains(al, "ad") || contains(al, "Mob")) {
+                        sb.append("Failed");
+                    } else if (contain(Step2.selected.packageInfo.services, "AD")
+                            || contain(Step2.selected.packageInfo.services, "Ad")
+                            || contain(Step2.selected.packageInfo.services, "Mob")
+                            || contain(Step2.selected.packageInfo.services, "ad")) {
+                        sb.append("Failed");
+                    } else {
+                        sb.append("Passed");
+                        good++;
+                    }
+                    sb.append("</p><hr />");
+                    //===================
+                    sb.append("<h2>综上所述，评定 " + Step2.selected.appName + " " + (int) (((float) good) / 6 * 100) + "%符合绿色公约要求。</h2><hr /><a href=\"http://green-android.org\">绿色公约官网</a><p>By w568w</p>");
+                    try {
+                        File_WritetoSDFrom_bytes(sb.toString().getBytes("UTF-8"), "result.html");
                     } catch (UnsupportedEncodingException e) {
                         e.printStackTrace();
                     }
-
                     tv.post(new Runnable() {
                         @Override
                         public void run() {
@@ -150,19 +164,29 @@ public class Step3 extends WizardStep {
             }).start();
         }
     }
-    public static String String_Between(String str, String leftstr, String rightstr)
-    {
-        if(!str.contains(leftstr)||!str.contains(rightstr))
+
+    public static ArrayList<String> getActivities(String pn) {
+        ArrayList<String> result = new ArrayList<>();
+        Intent intent = new Intent(Intent.ACTION_MAIN, null);
+        intent.setPackage(pn);
+        for (ResolveInfo info : MyApplication.getInstance().getPackageManager().queryIntentActivities(intent, 0)) {
+            result.add(info.activityInfo.name);
+        }
+        return result;
+    }
+
+    public static String String_Between(String str, String leftstr, String rightstr) {
+        if (!str.contains(leftstr) || !str.contains(rightstr))
             return "";
         int i = str.indexOf(leftstr) + leftstr.length();
-        return str.substring(i, str.indexOf(rightstr,i));
+        return str.substring(i, str.indexOf(rightstr, i));
     }
-    public static void File_WritetoSDFrom_bytes(byte[] data,String filename){
-        File f=new File(Environment.getExternalStorageDirectory().getPath()+"/"+filename);
-        if(f.exists())
+
+    public static void File_WritetoSDFrom_bytes(byte[] data, String filename) {
+        File f = new File(Environment.getExternalStorageDirectory().getPath() + "/" + filename);
+        if (f.exists())
             f.delete();
-        if(!f.exists())
-        {
+        if (!f.exists()) {
             try {
                 f.createNewFile();
             } catch (IOException e1) {
@@ -178,10 +202,20 @@ public class Step3 extends WizardStep {
             e.printStackTrace();
         }
     }
+
     private boolean contain(List<ActivityManager.RunningServiceInfo> array, String obj) {
-        if(array==null)return false;
+        if (array == null) return false;
         for (ActivityManager.RunningServiceInfo o : array) {
             if (!o.foreground && o.service.getPackageName().equals(obj))
+                return true;
+        }
+        return false;
+    }
+
+    private boolean contain(ServiceInfo[] a, String obj) {
+        if(a==null)return false;
+        for (ServiceInfo si : a) {
+            if (si.name.contains(obj) && si.enabled)
                 return true;
         }
         return false;
@@ -190,6 +224,15 @@ public class Step3 extends WizardStep {
     private <T> boolean contains(T[] a, T o) {
         if (a == null) return false;
         for (T b : a) {
+            if (o.equals(b))
+                return true;
+        }
+        return false;
+    }
+
+    private boolean contains(ArrayList a, Object o) {
+        if (a == null) return false;
+        for (Object b : a) {
             if (o.equals(b))
                 return true;
         }
